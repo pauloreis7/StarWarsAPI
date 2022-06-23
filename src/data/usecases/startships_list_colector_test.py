62f9049e9786b9d3ea365aeda022f68a5671750b
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-06-23 17:05:34 -0300

    feat: add spy mock to list starships

--- src/data/usecases/startships_list_colector_test.py
+++ src/data/usecases/startships_list_colector_test.py
@@ -1,11 +1,17 @@
-from src.infra import SwapiApiConsumer
-from .startships_list_colector import StarShipListColector
+from src.infra.tests.swapi_api_consumer import SwapiApiConsumerSpy
+from .startships_list_colector import StarshipsListColector
 
 
 def test_list():
-    api_consumer = SwapiApiConsumer()
-    star_ship_list_colector = StarShipListColector(api_consumer)
+    '''testing list method'''
+
+    api_consumer = SwapiApiConsumerSpy()
+    star_ship_list_colector = StarshipsListColector(api_consumer)
 
     page = 1
+    response = star_ship_list_colector.list(page)
 
-    star_ship_list_colector.list(page)
+    assert api_consumer.get_starships_attributes == {"page": page}
+    assert isinstance(response, list)
+    assert "id" in response[0]
+    assert "MGLT" in response[0]
